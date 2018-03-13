1962bead72d26181d9da8e1ccfbde61c581e2dc0
existmaster <existmaster@gmail.com>
2018-03-14 00:48:20 +0900

    test code

---
+++ src/test/java/com/existmaster/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.existmaster.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
